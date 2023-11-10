5ba7d8cc0ded5641c0fc9b96be83fc2d1806de2b
Nanamachi <7machi@nanamachi.net>
2023-11-10 15:57:34 +0900

    feat(content): html形式でも合わせてコピーできるようにする

--- content.js
+++ content.js
@@ -7,24 +7,62 @@ function addOnClickEvent(el) {
         if (!event.altKey) {
             return;
         }
-        navigator.clipboard.readText().then(function(text){
-            ticketKey = text.match(/([A-Z]+-[0-9]+)/)[0];
 
-            parsed = '['+text+'](' + BASE_URL + '/view/' + ticketKey + ')';
-
-            // クリップボードにコピー
-            navigator.clipboard.writeText(parsed);
-        });
+        if (event.ctrlKey || event.metaKey) {
+            // Alt + Ctrl (Win) または Alt + Cmd (Mac) の場合はHTML形式でコピー
+            navigator.clipboard.readText()
+            .then(formatToHtmlLink)
+            .then(writeText);
+        } else {
+            // Alt単体の場合はMarkDown形式でコピー
+            navigator.clipboard.readText()
+            .then(formatToMarkdownLink)
+            .then(writeText);
+        }
     });
     el.__mdlinker_injected = true;
 }
 
+function constructTicketUrl(text) {
+    return BASE_URL + '/view/' + text.match(/([A-Z]+-[0-9]+)/)[0];
+}
+
+function constructHtmlText(text) {
+    var ticketUrl = constructTicketUrl(text);
+    return '<a href="' + ticketUrl + '">' + text + '</a>';
+}
+
+function constructMarkdownText(text) {
+    var ticketUrl = constructTicketUrl(text);
+    return '[' + text + '](' + ticketUrl + ')';
+}
+
+function formatToMarkdownLink(copyText) {
+    return {
+        'text/html': new Blob([constructHtmlText(copyText)],{type: 'text/html'}),
+        'text/plain': new Blob([constructMarkdownText(copyText)],{type: 'text/plain'}),
+    };
+}
+
+function formatToHtmlLink(copyText) {
+    var text = constructHtmlText(copyText);
+    return {
+        'text/html': new Blob([text], {type: 'text/html'}),
+        'text/plain': new Blob([text], {type: 'text/plain'}),
+    };
+}
+
+function writeText(data) {
+    navigator.clipboard.write([new ClipboardItem(data)]);
+}
+
 function main() {
-    // .ticket__key-copy のクリック時に、クリップボードにコピーする
-    var trigElems = document.getElementsByClassName('copy-trigger');
+    // 一覧ページでのコピーボタン
+    var trigElems = document.querySelectorAll('.copy-trigger > ._trigger-text');
     if (trigElems.length > 0) {
         Array.from(trigElems).forEach(addOnClickEvent);
     }
+    // 詳細ページでのコピーボタン
     trigElems = document.getElementsByClassName('copy-key-btn');
     if (trigElems.length > 0) {
         Array.from(trigElems).forEach(addOnClickEvent);
